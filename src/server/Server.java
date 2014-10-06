3b54a6b8562fae5c198fffc236530ea1caa2f47e
mrzrm <m.leimgruebler@gmail.com>
2014-10-06 10:05:55 +0200

    ServerThread

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,10 +1,47 @@
 package server;
 
-public class Server {
+import java.awt.BorderLayout;
+import java.io.IOException;
+import java.net.ServerSocket;
 
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class Server extends JFrame{
+
+	private ServerSocket server;
+	private JTextArea output;
+	private static Server s;
+
+	public Server(){
+			
+		super("Gaggis Game-Server");
+	
+		try {
+			server = new ServerSocket(5000, 2);
+		}
+		catch( IOException e ) {
+			e.printStackTrace();
+			System.exit( 1 );
+		}
+
+		output = new JTextArea();
+		getContentPane().add( output, BorderLayout.CENTER );
+		output.setText( "Server waiting for connections\n" );
+
+		setLocation(100,100);
+		setSize(300,300);
+		setVisible(true);
+	}
+	
+	public void display(String s){
+		
+		output.append(s + "\n");
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		s = new Server();
 	}
 
 }
+
--- src/server/ServerThread.java
+++ src/server/ServerThread.java
@@ -1,15 +1,33 @@
 package server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 
 public class ServerThread extends Thread {
 	
-	private Socket socket = null;
-
+	private Socket connection;
+	private DataInputStream input;
+	private DataOutputStream output;
+	private Server server;
 
 	//Konstruktor
-	public ServerThread(Socket socket) {
-		this.socket  = socket;
+	public ServerThread(Socket socket, Server s) {
+		this.connection = socket;
+		
+		try {
+			input = new DataInputStream(connection.getInputStream() );
+			output = new DataOutputStream(connection.getOutputStream() );
+		}
+		catch( IOException e ) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		server = s;
+		
+		
 	}
 
 	//Run Methode
