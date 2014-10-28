5fe931f67d8e7bea6087cd98fa995a78cefccb7a
Globiglobi <adi.immer@bluewin.ch>
2014-10-28 15:43:14 +0100

    Pärchenstrassen OK

--- src/Gui/ZugVerifizieren.java
+++ src/Gui/ZugVerifizieren.java
@@ -14,6 +14,7 @@ public class ZugVerifizieren {
 		Collections.sort(zug, new CustomComparator());
 		// Collections.sort(gespielt, new CustomComparator());
 		int counterStrasse = 1;
+		int counterPaerchenStrasse = 0;
 
 		for (int i = 0; i < zug.size(); i++) {
 			System.out.println(zug.get(i).getWert());
@@ -25,57 +26,80 @@ public class ZugVerifizieren {
 		if (zug.size() == 1) {
 			System.out.println("einzelne karte gespielt");
 			return true;
-		} else if ((zug.size() == 2)
-				&& (zug.get(0).getWert() == zug.get(1).getWert())) {
+		}
+		if ((zug.size() == 2) && (zug.get(0).getWert() == zug.get(1).getWert())) {
 			System.out.println("paar gespielt");
 			return true;
-		} else if ((zug.size() == 3)
-				&& (zug.get(0).getWert() == zug.get(1).getWert())
+		}
+		if ((zug.size() == 3) && (zug.get(0).getWert() == zug.get(1).getWert())
 				&& (zug.get(0).getWert() == zug.get(2).getWert())) {
 			System.out.println("drilling gespielt");
 			return true;
-		} else if (zug.size() > 2) {
-			for (int i = zug.size()-1; i > 0; i--) {
-				if (zug.get(i).getWert() == zug.get(i - 1).getWert() + 1 && zug.get(i).getFarbe() == zug.get(i - 1).getFarbe()) {
+		}
+		if (zug.size() > 2) {
+			for (int i = zug.size() - 1; i > 0; i--) {
+				if (zug.get(i).getWert() == zug.get(i - 1).getWert() + 1
+						&& zug.get(i).getFarbe() == zug.get(i - 1).getFarbe()) {
 					counterStrasse++;
-				}
-				else{ 
-					return false;
+				} else {
+					break;
 				}
 			}
 			if (counterStrasse > 2) {
 				System.out.println(counterStrasse + "er Strasse gespielt");
 				return true;
 			}
-			   
-			else{
-			
+
+			else {
+
 			}
 
 		}
-		 else if ((zug.size() == 4) && (zug.get(0).getWert() ==
-		 zug.get(1).getWert()) && (zug.get(0).getWert() ==
-		 zug.get(2).getWert()) && (zug.get(0).getWert() ==
-		 zug.get(3).getWert())){
-		 System.out.println("vierling gespielt");
-		 return true;
-		 //wert von joker karte muss noch umgewandelt werden
-		 }else if ((zug.size() == 4) && (zug.get(0).getWert() ==
-		 zug.get(1).getWert()) && (zug.get(0).getWert() ==
-		 zug.get(2).getWert()) && (zug.get(0).getWert() ==
-		 zug.get(3).getWert()) && (zug.get(0).getWert() ==
-		 zug.get(4).getWert())){
-		 System.out.println("fuenfling gespielt");
-		 return true;
-		 }
-		 else if ((zug.size() == 4) && (zug.get(0).getWert() ==
-		 zug.get(1).getWert()) && (zug.get(2).getWert() ==
-		 zug.get(3).getWert()) && (zug.get(0).getFarbe() ==
-		 zug.get(2).getFarbe()) && zug.get(1).getFarbe() ==
-		 zug.get(3).getFarbe()){
-		 System.out.println("PaarStrasse gespielt");
-		 return true;
-		 }
+		if ((zug.size() == 4) && (zug.get(0).getWert() == zug.get(1).getWert())
+				&& (zug.get(0).getWert() == zug.get(2).getWert())
+				&& (zug.get(0).getWert() == zug.get(3).getWert())) {
+			System.out.println("vierling gespielt");
+			return true;
+			// wert von joker karte muss noch umgewandelt werden
+		}
+		// else if ((zug.size() == 4) && (zug.get(0).getWert() ==
+		// zug.get(1).getWert()) && (zug.get(0).getWert() ==
+		// zug.get(2).getWert()) && (zug.get(0).getWert() ==
+		// zug.get(3).getWert()) && (zug.get(0).getWert() ==
+		// zug.get(4).getWert())){
+		// System.out.println("fuenfling gespielt");
+		// return true;
+		// }
+		if (zug.size() >= 4) {
+			System.out.println("if paerchen");
+
+			int j = zug.size() - 1;
+			for (int i = zug.size() - 1; i > 0; i = i - 2) {
+				System.out.println("1. Schlaufe");
+				if ((zug.get(i).getWert() == zug.get(i - 1).getWert())
+						&& zug.get(j).getFarbe() == zug.get(j - 2).getFarbe()
+						&& zug.get(j).getWert() == zug.get(j - 2).getWert() + 1) {
+					counterPaerchenStrasse++;
+					j--;
+					System.out.println("counter" + counterPaerchenStrasse);
+				} else {
+					j = zug.size() - 1;
+					counterPaerchenStrasse = 1;
+					System.out.println("counter wieder 1");
+					break;
+				}
+			}
+			if (counterPaerchenStrasse > 1) {
+				System.out.println(counterPaerchenStrasse
+						+ "er Pärchenstrasse gespielt");
+				return true;
+			}
+
+			else {
+
+			}
+
+		}
 		// else if ((zug.size() == 4) && (zug.get(0).getWert() ==
 		// zug.get(2).getWert()) && (zug.get(1).getWert() ==
 		// zug.get(3).getWert()) && (zug.get(0).getFarbe() ==
